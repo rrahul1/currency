59f4f9a8de14cb2a6e4d716712287273231bd802
Rahul Singh <rathore.rahul.1999@gmail.com>
2023-12-15 16:19:50 +0530

    final commit

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -40,7 +40,7 @@ function Input({
         <select
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
           value={seletCurrency}
-          onAbort={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
           disabled={currencyDisable}
         >
           {currencyOptions.map((curr) => (
